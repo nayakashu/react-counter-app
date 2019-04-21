ae26a0062693e2f1f6ff34dfedb0e98aec4ccc0e
Ashutosh Nayak <ashutosh.nayak@sc.com>
2019-04-21 22:24:53 +0530

    Updating the state

--- src/counter/counter.jsx
+++ src/counter/counter.jsx
@@ -19,7 +19,7 @@ class Counter extends Component {
   }
 
   handleIncrement = () => {
-    console.log('Increment Clicked!', this);
+    this.setState({ count: this.state.count + 1 });
   };
 
   getBadgeClasses() {
