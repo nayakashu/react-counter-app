44041c331ff1916dbf795b508df637cc1c7d2283
Ashutosh Nayak <ashutosh.nayak@sc.com>
2019-04-21 21:05:27 +0530

    Adding style attribute - Part-2

--- src/counter/counter.jsx
+++ src/counter/counter.jsx
@@ -5,18 +5,13 @@ class Counter extends Component {
     count: 0
   };
 
-  styles = {
-    fontSize: 20,
-    fontWeight: 'bold'
-  };
-
   render() {
     return (
       <React.Fragment>
-        <span className='badge badge-primary m-2'>{this.formatCount()}</span>
-        <button style={this.styles} className='btn btn-secondary btn-sm'>
-          Increment
-        </button>
+        <span style={{ fontSize: 20 }} className='badge badge-primary m-2'>
+          {this.formatCount()}
+        </span>
+        <button className='btn btn-secondary btn-sm'>Increment</button>
       </React.Fragment>
     );
   }
