3c2e81cc8e9a7cd3646b7c3e7dc49aea557e080b
Ashutosh Nayak <ashutosh.nayak@sc.com>
2019-05-04 12:55:11 +0530

    Passing children to component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,6 +8,7 @@ class Counter extends Component {
   render() {
     return (
       <div>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement({ id: 1 })}
