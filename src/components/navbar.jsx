45e8410f115a23fbccda9c1474d0417e02b7719c
Ashutosh Nayak <ashutosh.nayak@sc.com>
2019-05-05 22:15:07 +0530

    Destructuring arguments like props

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 // Stateless Functional Component
-const NavBar = props => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a href="/#" className="navbar-brand">
         Todo Cart{' '}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
